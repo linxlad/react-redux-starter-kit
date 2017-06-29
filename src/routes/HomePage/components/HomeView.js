1617ea722fcfb91154b3046f740def44016a1f5a
Nathan Daly <nathand@openobjects.com>
2017-06-29 17:17:07 +0100

    Added semantic UI button which redirects to login page.

--- src/routes/HomePage/components/HomeView.js
+++ src/routes/HomePage/components/HomeView.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { browserHistory } from 'react-router';
 import './HomeView.scss';
 import PageLayout from '../../../layouts/PageLayout';
+import { Button } from 'semantic-ui-react';
 
 export const HomeView = () => (
   <div>
@@ -9,6 +11,10 @@ export const HomeView = () => (
         <h2>Home Page</h2>
         <h4>Fractal Project Structure Boilerplate</h4>
         <a href="https://github.com/davezuko/react-redux-starter-kit/wiki/Fractal-Project-Structure">Read more.</a>
+        <p></p>
+        <p>
+          <Button onClick={() => browserHistory.push('/login')} size='large' color='green'>Login</Button>
+        </p>
       </div>
     </PageLayout>
   </div>
